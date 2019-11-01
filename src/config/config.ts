84fe6dd2d09afe1e21550c2ad7073c19e01ee24a
zakariaBoukernafa <zakaria0697@gmail.com>
2019-11-01 17:33:54 +0100

    clean up

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "zakariadatabase",
+    "password": "*******",
+    "database": "zakariadatabase",
+    "host": "zakariadatabase.cxwfylzspc5q.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "zakariadatabase-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
