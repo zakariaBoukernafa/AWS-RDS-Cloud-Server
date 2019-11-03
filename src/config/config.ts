03afbfaf05938ebd93711a975fc10af11131aceb
zakariaBoukernafa <zakaria0697@gmail.com>
2019-11-03 16:22:38 +0100

    elastic beanstalk implementation

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
   "dev": {
-    "username": "zakariadatabase",
-    "password": "*******",
-    "database": "zakariadatabase",
-    "host": "zakariadatabase.cxwfylzspc5q.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "zakariadatabase-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres"
   }
 }
